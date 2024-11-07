5c9701d8e8387887dc5d6a02a13b5eb999be32ea
Techatrix <techatrix@mailbox.org>
2024-11-07 19:02:07 +0100

    increase log buffer size from 1024 to 4096
    This doesn't usually make a difference except when using --enable-message-tracing

--- src/main.zig
+++ src/main.zig
@@ -68,7 +68,7 @@ fn logFn(
     const scope_txt: []const u8 = comptime @tagName(scope);
     const trimmed_scope = if (comptime std.mem.startsWith(u8, scope_txt, "_")) scope_txt[1..] else scope_txt;
 
-    var buffer: [1024]u8 = undefined;
+    var buffer: [4096]u8 = undefined;
     var fbs = std.io.fixedBufferStream(&buffer);
     const no_space_left = blk: {
         fbs.writer().print("<{s:^6}> {s}: ", .{ trimmed_scope, level_txt }) catch break :blk true;
