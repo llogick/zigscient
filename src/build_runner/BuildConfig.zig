8d2262ef07e4cd2dbea4eec68edda7109551a13c
llogick <16590917+llogick@users.noreply.github.com>
2024-11-30 08:48:37 -0800

    Extend CompileSteps collection
    Push local, ie nearest to the current build.zig, and those with most modules towards the top.

--- src/build_runner/BuildConfig.zig
+++ src/build_runner/BuildConfig.zig
@@ -2,14 +2,14 @@ const std = @import("std");
 
 roots_info_file: []const u8,
 deps_build_roots: []NamePathPair,
-roots: [][]NamePathPair,
+roots: []RootEntry,
 packages: []NamePathPair,
 include_dirs: []const []const u8,
 top_level_steps: []const []const u8,
 available_options: std.json.ArrayHashMap(AvailableOption),
 
 pub const RootEntry = struct {
-    path: []const u8,
+    name: []const u8,
     mods: []NamePathPair,
 };
 pub const NamePathPair = struct {
