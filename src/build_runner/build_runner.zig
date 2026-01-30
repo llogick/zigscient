0c44b964ab356783d81362532b7f8ca357ca8168
llogick <16590917+llogick@users.noreply.github.com>
2026-01-30 02:19:16 -0800

    Zig 0.16.0-dev.2365+377bb8f23
    "std: migrate getcwd to Io"

--- src/build_runner/build_runner.zig
+++ src/build_runner/build_runner.zig
@@ -98,7 +98,7 @@ pub fn main(init: process.Init.Minimal) !void {
             .io = io,
             .gpa = arena,
             .manifest_dir = try local_cache_directory.handle.createDirPathOpen(io, "h", .{}),
-            .cwd = try process.getCwdAlloc(single_threaded_arena.allocator()),
+            .cwd = try process.currentPathAlloc(io, single_threaded_arena.allocator()),
         },
         .zig_exe = zig_exe,
         .environ_map = try init.environ.createMap(arena),
