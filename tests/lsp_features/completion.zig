ea4a17ee392ad75b2f70208a9f7c6c4afacd1b72
Techatrix <techatrix@mailbox.org>
2026-02-26 09:51:49 +0100

    fix enum completion on out of bound token index

--- tests/lsp_features/completion.zig
+++ tests/lsp_features/completion.zig
@@ -3650,6 +3650,9 @@ test "enum completion on out of bound token index" {
     try testCompletion(
         \\ = 1.<cursor>
     , &.{});
+    try testCompletion(
+        \\) { .<cursor>
+    , &.{});
 }
 
 test "combine doc comments of declaration and definition" {
