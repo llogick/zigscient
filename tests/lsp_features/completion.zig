7a6e34c4bc499fae0ddf643f9a66d12dad959fe3
Techatrix <techatrix@mailbox.org>
2026-02-26 07:42:14 +0100

    fix import completions after unterminted string literal with newline

--- tests/lsp_features/completion.zig
+++ tests/lsp_features/completion.zig
@@ -3736,6 +3736,13 @@ test "filesystem string literal ends with non ASCII symbol" {
     });
 }
 
+test "filesystem unterminated string literal with newline" {
+    try testCompletion(
+        \\const foo = @import("
+        \\  <cursor>
+    , &.{});
+}
+
 test "label details disabled" {
     try testCompletionWithOptions(
         \\const S = struct {
