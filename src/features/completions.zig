ff8cedddbda81ff064b679ff21b76c9b921fa9b0
llogick <16590917+llogick@users.noreply.github.com>
2026-04-26 00:13:12 -0700

    Fix .import_string_literal completions

--- src/features/completions.zig
+++ src/features/completions.zig
@@ -926,7 +926,6 @@ fn completeError(builder: *Builder, token_index: std.zig.Ast.TokenIndex) Analyse
 fn completeFileSystemStringLiteral(builder: *Builder, pos_context: Analyser.PositionContext) Analyser.Error!void {
     const io = builder.server.io;
 
-    var completions: CompletionSet = .empty;
     const store = &builder.server.document_store;
     const source = builder.orig_handle.tree.source;
 
@@ -984,26 +983,26 @@ fn completeFileSystemStringLiteral(builder: *Builder, pos_context: Analyser.Posi
 
             try builder.completions.ensureUnusedCapacity(builder.arena, build_config.deps_build_roots.len);
             for (build_config.deps_build_roots) |dbr| {
-                completions.putAssumeCapacity(.{
+                builder.completions.appendAssumeCapacity(.{
                     .label = dbr.name,
                     .kind = .Module,
                     .detail = dbr.path,
                     .sortText = try generateSortText(builder.arena, 4, dbr.name),
-                }, {});
+                });
             }
         } else if (try builder.orig_handle.getAssociatedBuildFileUri(store)) |uri| blk: {
             const build_file = store.getBuildFile(uri).?;
             const build_config = build_file.tryLockConfig(store.io) orelse break :blk;
             defer build_file.unlockConfig(store.io);
 
-            try completions.ensureUnusedCapacity(builder.arena, build_config.packages.len);
+            try builder.completions.ensureUnusedCapacity(builder.arena, build_config.packages.len);
             for (build_config.packages) |pkg| {
-                completions.putAssumeCapacity(.{
+                builder.completions.appendAssumeCapacity(.{
                     .label = pkg.name,
                     .kind = .Module,
                     .detail = pkg.path,
                     .sortText = try generateSortText(builder.arena, 4, pkg.name),
-                }, {});
+                });
             }
         }
 
